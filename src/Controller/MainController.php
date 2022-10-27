fbfafbb81f347fff7dc39fbdd7259b87fcd6a3b2
romain2518 <rolenoir25@gmail.com>
2022-10-27 14:59:40 +0200

    Add about, legal mentions, patch notes & back office pages; Add links to home page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,9 +8,33 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'app_main_home')]
     public function home(): Response
     {
         return $this->render('main/home.html.twig');
+    }
+
+    #[Route('/about', name: 'app_main_about')]
+    public function about(): Response
+    {
+        return $this->render('main/about.html.twig');
+    }
+
+    #[Route('/legal-mentions', name: 'app_main_legal-mentions')]
+    public function legalMentions(): Response
+    {
+        return $this->render('main/legal_mentions.html.twig');
+    }
+
+    #[Route('/patch-notes', name: 'app_main_patch-notes')]
+    public function patchNotes(): Response
+    {
+        return $this->render('main/patch_notes.html.twig');
+    }
+
+    #[Route('/back-office', name: 'app_main_back-office')]
+    public function backOffice(): Response
+    {
+        return $this->render('main/back_office.html.twig');
     }
 }
