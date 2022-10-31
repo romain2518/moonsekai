397ccd6757b22f2628ef1c5fe83aaeb446ca6957
romain2518 <rolenoir25@gmail.com>
2022-10-31 15:13:59 +0100

    Add reset-password feature (send a email to change password in case of forgotten password)

---
+++ src/Form/ChangePasswordFormType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form;
+
+use Rollerworks\Component\PasswordStrength\Validator\Constraints\PasswordStrength;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotCompromisedPassword;
+
+class ChangePasswordFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'options' => [
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
+                'first_options' => [
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Please enter a password',
+                        ]),
+                        new Length([
+                            'min' => 8,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                        new NotCompromisedPassword([
+                            'message' => 'This password has been leaked in a data breach, it must not be used. Please use another password.',
+                        ]),
+                        new PasswordStrength([
+                            'minStrength' => 4,
+                            // @see https://github.com/rollerworks/PasswordStrengthValidator/blob/main/docs/strength-validation.md
+                            'message' => 'This password is too weak, it should conatains both lowercase and uppercase alpha characters, a digit and a special character.',
+                        ]),
+                    ],
+                    'label' => 'New password',
+                ],
+                'second_options' => [
+                    'label' => 'Repeat Password',
+                ],
+                'invalid_message' => 'The password fields must match.',
+                // Instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
