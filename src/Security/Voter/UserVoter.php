183bda922c87c5d63ad1c95de158068980b2d3f0
romain2518 <rolenoir25@gmail.com>
2022-11-01 17:01:26 +0100

    Add edit rank feature

--- src/Security/Voter/UserVoter.php
+++ src/Security/Voter/UserVoter.php
@@ -15,7 +15,7 @@ class UserVoter extends Voter
 {
     public const EDIT_PROFILE = 'USER_EDIT_PROFILE';
     public const EDIT_USER = 'USER_EDIT';
-    public const EDIT_RANK = 'USER_EDIT_RANK';
+    public const EDIT_USER_RANK = 'USER_EDIT_RANK';
 
     public function __construct(
         private AccessDecisionManagerInterface $accessDecisionManager,
@@ -27,8 +27,9 @@ class UserVoter extends Voter
     {
         // replace with your own logic
         // https://symfony.com/doc/current/security/voters.html
-        return in_array($attribute, [self::EDIT_PROFILE, self::EDIT_USER, self::EDIT_RANK])
-            && $subject instanceof \App\Entity\User;
+        return (in_array($attribute, [self::EDIT_PROFILE, self::EDIT_USER])
+            && $subject instanceof \App\Entity\User)
+            || ($attribute === self::EDIT_USER_RANK && is_string($subject));
     }
 
     protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
@@ -55,11 +56,22 @@ class UserVoter extends Voter
                 if ($this->security->isGranted('ROLE_SUPERADMIN')) $maxRole = 'ROLE_SUPERADMIN';
 
                 $token = new UsernamePasswordToken($subject, 'none', $subject->getRoles());
-                return !$this->accessDecisionManager->decide($token, [$maxRole], $subject); // Return true if the requested user is not granted the ROLE_MODERATOR
+                
+                // Return true if the requested user has a lower role than the logged in user
+                return !$this->accessDecisionManager->decide($token, [$maxRole], $subject);
                 
                 break; 
-            case self::EDIT_RANK:
-                # code...
+            case self::EDIT_USER_RANK:
+                /** @var string $subject role that must be lower than the one logged in user has */
+                
+                if ($subject === 'ROLE_USER') {
+                    return $this->security->isGranted('ROLE_MODERATOR');
+                } elseif ($subject === 'ROLE_MODERATOR') {
+                    return $this->security->isGranted('ROLE_ADMIN');
+                } else { // $subject === 'ROLE_ADMIN'
+                    return $this->security->isGranted('ROLE_SUPERADMIN');
+                }
+                
                 break;
         }
 
