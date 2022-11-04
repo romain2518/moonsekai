057769bfbbf2128d4de5e9f27308636020d063dd
romain2518 <rolenoir25@gmail.com>
2022-11-04 12:25:59 +0100

    Add platform crud

---
+++ src/Form/PlatformType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Platform;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class PlatformType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('url')
+            ->add('pictureFile', VichImageType::class, [
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Platform::class,
+        ]);
+    }
+}
