6b3ad5874b32d434b6386d3bccd1ece2cd1822e2
romain2518 <rolenoir25@gmail.com>
2022-11-04 20:59:13 +0100

    Add work crud

--- src/Entity/Work.php
+++ src/Entity/Work.php
@@ -8,10 +8,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: WorkRepository::class)]
+#[Vich\Uploadable]
 #[ORM\Index(name: 'idx_search', fields: ['name', 'originalName'])]
 #[ORM\Index(name: 'idx_advanced_search', fields: ['name', 'type', 'nativeCountry', 'originalName'])]
 #[ORM\HasLifecycleCallbacks]
@@ -33,12 +36,7 @@ class Work
     private ?string $name = null;
 
     #[ORM\Column(length: 20)]
-    #[Assert\Choice([
-        'shōnen',
-        'seinen',
-        'shōjo',
-        'josei',
-    ])]
+    #[Assert\Choice(callback: 'getTypes')]
     #[Assert\NotBlank]
     private ?string $type = null;
     
@@ -62,6 +60,16 @@ class Work
         new Assert\NotBlank,
     ])]
     private ?array $alternativeName = [];
+
+    #[Vich\UploadableField(mapping: 'work_pictures', fileNameProperty: 'picturePath')]
+    #[Assert\File(
+        maxSize: "5M",
+        mimeTypes: ["image/jpeg", "image/png"],
+        maxSizeMessage: "The maximum allowed file size is 5MB.",
+        mimeTypesMessage: "Only png, jpg and jpeg images are allowed."
+    )]
+    #[CustomAssert\NotBlankVich(message: 'Please provide a picture to create a work.', target: 'picturePath')]
+    private ?File $pictureFile = null;
 
     #[ORM\Column(length: 255)]
     #[Groups('api_user_show')]
@@ -115,6 +123,16 @@ class Work
         $this->tags = new ArrayCollection();
         $this->platforms = new ArrayCollection();
         $this->followers = new ArrayCollection();
+    }
+
+    public static function getTypes()
+    {
+        return [
+            'shōnen',
+            'seinen',
+            'shōjo',
+            'josei',
+        ];
     }
 
     public function getId(): ?int
@@ -182,12 +200,29 @@ class Work
         return $this;
     }
 
+    public function getPictureFile(): ?File
+    {
+        return $this->pictureFile;
+    }
+
+    public function setPictureFile(?File $pictureFile = null): self
+    {
+        $this->pictureFile = $pictureFile;
+
+        if (null !== $pictureFile) {
+            // Needed to trigger event listener
+            $this->updatedAt = new \DateTime();
+        }
+
+        return $this;
+    }
+
     public function getPicturePath(): ?string
     {
         return $this->picturePath;
     }
 
-    public function setPicturePath(string $picturePath): self
+    public function setPicturePath(?string $picturePath): self
     {
         $this->picturePath = $picturePath;
 
