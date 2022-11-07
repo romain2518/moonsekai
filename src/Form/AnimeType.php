5c2698334a9f1e8e350a4d9630620ace29dfe2f6
romain2518 <rolenoir25@gmail.com>
2022-11-07 18:31:53 +0100

    Add anime crud

---
+++ src/Form/AnimeType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Anime;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Range;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class AnimeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('state', ChoiceType::class, [
+                'choices' => Anime::getStates(),
+                'choice_label' => function ($choice)
+                {
+                    return ucfirst($choice);
+                },
+            ])
+            ->add('author')
+            ->add('animationStudio')
+            ->add('releaseYear')
+            ->add('pictureFile', VichImageType::class, [
+                'required' => false,
+                'allow_delete' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Anime::class,
+        ]);
+    }
+}
