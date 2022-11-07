fbb25f24f35ce50ae39adb0a5f75df04a10cef0d
romain2518 <rolenoir25@gmail.com>
2022-11-07 21:06:25 +0100

    Add Season vich mapping

--- src/Entity/Season.php
+++ src/Entity/Season.php
@@ -7,9 +7,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: SeasonRepository::class)]
+#[Vich\Uploadable]
 #[ORM\HasLifecycleCallbacks]
 class Season
 {
@@ -31,6 +34,15 @@ class Season
         max: 50,
     )]
     private ?string $name = null;
+
+    #[Vich\UploadableField(mapping: 'season_pictures', fileNameProperty: 'picturePath')]
+    #[Assert\File(
+        maxSize: "5M",
+        mimeTypes: ["image/jpeg", "image/png"],
+        maxSizeMessage: "The maximum allowed file size is 5MB.",
+        mimeTypesMessage: "Only png, jpg and jpeg images are allowed."
+    )]
+    private ?File $pictureFile = null;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $picturePath = null;
@@ -82,6 +94,23 @@ class Season
     public function setName(?string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPictureFile(): ?File
+    {
+        return $this->pictureFile;
+    }
+
+    public function setPictureFile(?File $pictureFile = null): self
+    {
+        $this->pictureFile = $pictureFile;
+
+        if (null !== $pictureFile) {
+            // Needed to trigger event listener
+            $this->updatedAt = new \DateTime();
+        }
 
         return $this;
     }
