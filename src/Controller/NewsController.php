8ac3a93ffd70bd1c41079cddb4eacae3389e9956
romain2518 <rolenoir25@gmail.com>
2022-11-17 12:10:23 +0100

    Add app_news_show route

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -12,10 +12,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-#[Route('back-office/news')]
 class NewsController extends AbstractController
 {
-    #[Route('/{limit}/{offset}', name: 'app_news_index', requirements: ['limit' => '\d+', 'offset' => '\d+'], methods: ['GET'])]
+    #[Route('/back-office/news/{limit}/{offset}', name: 'app_news_index', requirements: ['limit' => '\d+', 'offset' => '\d+'], methods: ['GET'])]
     public function index(NewsRepository $newsRepository, int $limit = 20, int $offset = 0): Response
     {
         return $this->render('news/index.html.twig', [
@@ -23,7 +22,7 @@ class NewsController extends AbstractController
         ]);
     }
 
-    #[Route('/add', name: 'app_news_new', methods: ['GET', 'POST'])]
+    #[Route('/back-office/news/add', name: 'app_news_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager, UserInterface $user): Response
     {
         $news = new News();
@@ -45,7 +44,19 @@ class NewsController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_news_edit', methods: ['GET', 'POST'])]
+    #[Route('news/{id}', name: 'app_news_show', methods: ['GET'])]
+    public function show(News $news = null): Response
+    {
+        if (null === $news) {
+            throw $this->createNotFoundException('News not found.');
+        }
+
+        return $this->render('news/show.html.twig', [
+            'news' => $news,
+        ]);
+    }
+
+    #[Route('/back-office/news/{id}/edit', name: 'app_news_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, News $news = null, EntityManagerInterface $entityManager, UserInterface $user): Response
     {
         if (null === $news) {
@@ -69,7 +80,7 @@ class NewsController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/delete', name: 'app_news_delete', methods: ['POST'])]
+    #[Route('/back-office/news/{id}/delete', name: 'app_news_delete', methods: ['POST'])]
     public function delete(Request $request, News $news = null, EntityManagerInterface $entityManager): Response
     {
         if (null === $news) {
